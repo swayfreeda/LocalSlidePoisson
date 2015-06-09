4b3a6ef497fa333e9403fd5a5419a74ac345533d
Luwei Yang <corsy@me.com>
2015-06-09 13:24:55 +0800

    Added MarchingCube

---
+++ include/IsoSurfaceExtractor.h
@@ -0,0 +1,16 @@
+//
+// Created by Luwei Yang on 6/9/15.
+//
+
+#ifndef RECONSTRUCTOR_ISOSURFACEEXTRACTOR_H
+#define RECONSTRUCTOR_ISOSURFACEEXTRACTOR_H
+
+#include <Mesh.h>
+
+class IsoSurfaceExtractor {
+public:
+     DDG::Mesh* extractVoxel();
+};
+
+
+#endif //RECONSTRUCTOR_ISOSURFACEEXTRACTOR_H
---
+++ src/IsoSurfaceExtractor.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Luwei Yang on 6/9/15.
+//
+
+#include "IsoSurfaceExtractor.h"
