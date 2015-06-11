52cfe25c6f41b2f2ad7db0054d7f31541cd96f5f
Luwei Yang <corsy@me.com>
2015-06-11 16:11:03 +0800

    Added CG solver

--- include/LinearSolver.h
+++ include/LinearSolver.h
@@ -8,7 +8,6 @@
 #include <Eigen/Sparse>
 #include <Eigen/Dense>
 
-using namespace Eigen;
 
 // SolverType
 enum SolverType
@@ -27,12 +26,25 @@ enum ErrorType
     kMatrixSingularError,
 };
 
-template <class T>
+template <typename T>
 class LinearSolver
 {
 public:
     // Solve Ax = b
-    //static int solve(const SparseMatrix<T>& A, const Matrix<T, Dynamic, 1>  &b, Matrix<T, Dynamic, 1> &x);
+    // static int solve(const SparseMatrix<T>& A, const Matrix<T, Eigen::Dynamic, 1>  &b, Matrix<T, Eigen::Dynamic, 1> &x);
+};
+
+template <typename T>
+class CGSolver:public LinearSolver<T>
+{
+public:
+    // Solve Ax = b via interation
+    static int solve(const Eigen::SparseMatrix<T> &A, const Eigen::VectorXd &b, Eigen::VectorXd &x);
+
+    // Solve Ax = b via iteration with external parameter of max iteration
+    // and torlerance
+    //static int solve(const Eigen::SparseMatrix<T> &A, const Eigen::Matrix<T, Eigen::Dynamic, 1> &b, Eigen::Matrix<T, Eigen::Dynamic, 1> &x,
+    //              const int maxIteration, const double torlerance);
 };
 
 
